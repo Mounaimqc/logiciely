d6a740cb16b01b1b711645cecbbf98a6efb04fa7
Mounaimqc <160617744+Mounaimqc@users.noreply.github.com>
2026-07-24 16:04:30 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,7 +36,7 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc030","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
-  config: {
-    renderer: 'html'
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "2382793398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
